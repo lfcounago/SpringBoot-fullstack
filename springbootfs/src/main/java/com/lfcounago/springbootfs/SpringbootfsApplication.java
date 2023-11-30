8566f7c2fd7b3893fb9f7576211f5826b26f53d8
Luis Fernández Couñago <lfcounago@gradiant.org>
2023-11-30 23:42:56 +0100

    Initial commit

---
+++ springbootfs/src/main/java/com/lfcounago/springbootfs/SpringbootfsApplication.java
@@ -0,0 +1,13 @@
+package com.lfcounago.springbootfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfsApplication.class, args);
+	}
+
+}
---
+++ springbootfs/src/test/java/com/lfcounago/springbootfs/SpringbootfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfcounago.springbootfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
